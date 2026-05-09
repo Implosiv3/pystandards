34f24328134a1c3f32d55f4b098ccdbbb796cfe2
danialcala94 <danielalcalavalera@gmail.com>
2026-05-09 18:23:56 +0700

    Added 'lang.py' module. Added new tests. New version.

--- tests/test_mixed.py
+++ tests/test_mixed.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.mandatory
-def test_mandatory():
+def test_http():
     from pystandards.http import HttpContentType, HttpMethod
     
     assert HttpContentType.APPLICATION_JSON.value == 'application/json'
@@ -12,3 +12,11 @@ def test_mandatory():
     assert HttpMethod.GET.value == 'GET'
     assert HttpMethod.GET.name == 'GET'
     assert HttpMethod.to_enum('POST') == HttpMethod.POST
+
+@pytest.mark.mandatory
+def test_lang():
+    from pystandards.lang import LanguageISO639
+
+    assert LanguageISO639.SPANISH.value == 'es'
+    assert LanguageISO639.ENGLISH.name == 'ENGLISH'
+    assert LanguageISO639.to_enum('it') == LanguageISO639.ITALIAN
